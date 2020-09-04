457ae14e0b464456c8adf63fa99723f50793290e
Valeria <70730047+valeria-scherb@users.noreply.github.com>
2020-09-04 03:12:08 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,4 +6,4 @@ def add(first_term, second_term):
     return first_term + second_term
 
 def subtract(first_term, second_term):
-    return first_term - second_term
+    return first_term + second_term
